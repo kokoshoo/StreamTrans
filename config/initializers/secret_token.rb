016ff30c071fc9d3eed13b9d7f74dbeab217e1ca
Justin Rodriguez <kokoshoo@gmail.com>
2013-07-13 18:05:24 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreamTrans::Application.config.secret_token = '939c56d852992dcb4ce720c741286a0435392b2c39f5c45629380a5b2ce770539d3fafd90c3fcec4f2ff1305a9e6d8cf17bd95c154ed52e36ee172f8d18b8872'
